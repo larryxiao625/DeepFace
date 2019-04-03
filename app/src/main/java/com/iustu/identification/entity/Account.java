0865027baf15853e10e499fbebdd609b9fe86d76
苏光辉 <sgh2810@163.com>
2019-04-03 18:38:42 +0800

    完善Account

--- app/src/main/java/com/iustu/identification/entity/Account.java
+++ app/src/main/java/com/iustu/identification/entity/Account.java
@@ -1,5 +1,8 @@
 package com.iustu.identification.entity;
 
+import com.google.gson.Gson;
+import com.iustu.identification.util.MSP;
+
 import retrofit2.http.GET;
 
 /**
@@ -8,6 +11,32 @@ import retrofit2.http.GET;
  * 用来记录登录账户的数据表
  */
 public class Account {
-    String name;     // 作为主键
-    String password;
+    public String name;     // 作为主键
+    public String password;
+
+    // 将对象转化为json字符串，存进SharePreference中
+    private String toJsonString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    // 将json字符串转化为ParameterConfig对象
+    // 主要是从SharePreference中取出String后转化为对象
+    private static Account fromJsonString(String json) {
+        if (json == null)
+            return null;
+        Gson gson = new Gson();
+        return gson.fromJson(json, Account.class);
+    }
+
+    // 将ParameterConfig对象转化为json字符串之后保存到SharePreference中
+    public void save() {
+        String jsonString = this.toJsonString();
+        MSP.getInstance(MSP.SP_ACCOUNT).edit().putString(MSP.ACCOUNT, jsonString).commit();
+    }
+
+    // 从json中解析对象并返回
+    public static Account getFromSP() {
+        return fromJsonString(MSP.getInstance(MSP.SP_ACCOUNT).getString(MSP.ACCOUNT, null));
+    }
 }
